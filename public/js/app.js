bc9f4d49554ec8c9fa37881f425e8e4738a5a78d
unknown <you@example.com>
2021-11-25 11:42:14 +0530

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-console.log('client side javascript')
-
-
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageone = document.querySelector('#message-1')
